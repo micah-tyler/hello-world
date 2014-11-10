664e00bc08a786ed229b3fbe425ed6f7f38cd44d
micah-tyler <micah.tyler2010@gmail.com>
2014-11-10 13:25:45 -0800

    Made hello world for c, python, and java.

---
+++ java/hello.java
@@ -0,0 +1,9 @@
+class hello {
+	public static void main(String[] args) {
+		if(args.length!=0) {
+			System.out.println("Hello world "+args[0]); // Display the string.
+		} else {
+			System.out.println("Hello world");
+		}
+	}
+}
